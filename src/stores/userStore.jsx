5777379fa54f29104905ea030205a7c0d4026d22
juan <juanbejamingiupponi@gmail.com>
2024-04-26 09:41:57 -0300

    26/4 build for vercel deploy

--- src/stores/userStore.jsx
+++ src/stores/userStore.jsx
@@ -1,6 +1,6 @@
-import { useStore } from "zustand";
+import {create} from 'zustand';
 
-export const useUserStore = useStore((set) => ({
+export const useUserStore = create((set) => ({
     user: null,
     login: (user) => set({ user }),
     logout: () => set({ user: null }),
